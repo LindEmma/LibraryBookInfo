ace3d74f52509cd710cae409a6db8949dcb49e0a
Emma <elune.lind@gmail.com>
2023-10-05 12:36:02 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,21 @@
+﻿namespace LibraryBookInfo
+{
+    internal class Book
+    {
+        public string _title;
+        public string _author;
+        public string _summary;
+
+        public Book(string title, string author, string summary)
+        {
+            _title = title;
+            _author = author;
+            _summary = summary;
+        }
+        public void PrintBookInfo()
+        {
+            Console.WriteLine("\nBoken {0} är skriven av {1} och kan sammanfattas som: \n{2}",
+                _title, _author, _summary);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LibraryBookInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hej, välkommen till ditt personliga bibliotek!\nSkriv titeln på din bok: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Skriv bokens författare: ");
+            string author = Console.ReadLine();
+
+            Console.Write("Skriv en kort sammanfattning av boken: ");
+            string summary = Console.ReadLine();
+
+            Book book1 = new Book(title, author, summary);
+
+            book1.PrintBookInfo();
+
+        }
+    }
+}
